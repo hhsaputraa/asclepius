99c20060166d6c8f2c187a274f649f524a4d9bb4
hhsaputraa <hhsptr12@gmail.com>
2024-05-08 09:02:02 +0700

    Push code to remote repository

---
+++ src/server/routes.js
@@ -0,0 +1,25 @@
+const postPredictHandler = require('../server/handler');
+ 
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        maxBytes: 1000000,
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  },
+  {
+    path: '/predict/histories',
+    method: 'GET',
+    handler: (request, h) => {
+      return 'Ini adalah metode GET untuk /predict';
+    }
+  }
+]
+ 
+module.exports = routes;
